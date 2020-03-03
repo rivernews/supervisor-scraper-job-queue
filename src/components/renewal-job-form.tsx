8ccbe6d3f15f329156dbc86a5f5c77877c49ac14
Shaung Cheng <shaungc@umich.edu>
2020-03-03 13:11:42 -0800

    commit in progress work

---
+++ src/components/renewal-job-form.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Formik } from 'formik';
+
+export enum ScraperMode {
+    REGULAR = 'regular',
+    RENEWAL = 'renewal'
+}
+
+interface ScraperCrossRequestData {
+    orgId: string;
+    orgName: string;
+    lastProgress: ScraperProgressData;
+    lastReviewPage: string;
+    scrapeMode: ScraperMode;
+};
+
+export interface ScraperProgressData {
+    // used in all cases
+    processed: number;
+    wentThrough: number;
+    total: number;
+
+    // used in FINISH and propogate back progress to schedule cross session job
+    durationInMilli: string;
+    page: number;
+    processedSession: number;
+}
+
+export const RenewalJobForm = () => (
+    <div>
+      <h1>Create a Renewal Job</h1>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
